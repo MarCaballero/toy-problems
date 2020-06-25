0861720ecc60eb1dde55dc1587af92797f95d8af
Maria Melissa Caballero <mariamelissacaballero@MacBook-Pro.local>
2020-06-24 22:08:29 -0400

    My First Commit

--- coinSums/coinSums.js
+++ coinSums/coinSums.js
@@ -22,4 +22,6 @@ example usage of `makeChange`:
 makeChange(1) === 1
 // aka, there's only two ways to make 2p. that's with two, 1p pieces or with a single 2p piece
 makeChange(2) === 2
+
+Example Change
 */
