88a0526d451735dec08eb5894a24a9d5d91445b4
MarCaballero <caballeromelissa84@gmail.com>
2020-07-13 19:29:09 -0400

    Array of Even Numbers

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -9,3 +9,19 @@
  * var onlyEven = evenOccurence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]);
  * console.log(onlyEven); //  4
 */
+
+function onlyEven (arrayOfNumbers){
+    var newArr = [];
+    for (var i = 0; i < arrayOfNumbers.length; i++){
+        if(arrayOfNumbers[i] % 2 === 0){
+           newArr.push(arrayOfNumbers[i]);
+        }
+    } 
+    if (newArr.length === 0){
+        newArr = null;
+    }
+    return newArr;
+}
+
+
+
